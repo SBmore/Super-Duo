dd2b88fa03c3ce446f295e3ff76377545a19e9f2
SBmore <s.blakemore87@gmail.com>
2015-10-11 22:05:03 +0100

    Refactor: Changed file name of Widget provider to match class

--- FootballScores/app/src/main/java/barqsoft/footballscores/Widgets/WidgetDataProvider.java
+++ FootballScores/app/src/main/java/barqsoft/footballscores/Widgets/WidgetDataProvider.java
@@ -43,7 +43,7 @@ public class WidgetDataProvider implements RemoteViewsFactory {
     public RemoteViews getViewAt(int position) {
         RemoteViews mView = new RemoteViews(mContext.getPackageName(),
                 android.R.layout.simple_list_item_1);
-        mView.setTextViewText(android.R.id.text1, mCollections.get(position));
+//        mView.setTextViewText(android.R.id.text1, mCollections.get(position));
         mView.setTextColor(android.R.id.text1, Color.BLACK);
         return mView;
     }
